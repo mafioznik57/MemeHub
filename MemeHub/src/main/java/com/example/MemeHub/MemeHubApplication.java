5a9772515f8fd787b3b1580fa20c7b5e2672cce6
Erasyl <erasylkabdrash032@gmail.com>
2025-09-20 22:46:01 +0500

    First commit

---
+++ MemeHub/src/main/java/com/example/MemeHub/MemeHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.MemeHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeHubApplication.class, args);
+	}
+
+}
---
+++ MemeHub/src/test/java/com/example/MemeHub/MemeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MemeHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
